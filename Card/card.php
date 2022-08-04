3e296f1271b2443401915b61777bf606a472f942
Chessdipeshgiri <110391744+Chessdipeshgiri@users.noreply.github.com>
2022-08-04 08:28:23 +0545

    Add files via upload

---
+++ Card/card.php
@@ -0,0 +1,43 @@
+<div class="container p-2">
+<p class="text-center"><strong>FREE Online Objective Questions Test</strong></p>
+<p class="text-center">
+MCQBES provides the online based FREE practice test (mock test) for the students preparing for CMAT (TU), BCA (TU). 
+  To start the test please register, login and select your desired test.</p>
+</div>
+
+<div class="container p-1">
+  <div class="row">
+    <div class="col-4">
+      <div class="card">
+        <img src="img/card/card2.jpg" class="card-img-top" alt="BCA (TU)">
+          <div class="card-body">
+            <h5 class="card-title">Board/University:</h5>
+            <p class="card-text">TU (Tribhuvan University)</p>
+            <a href="login.php" class="btn btn-primary">Login</a>
+          </div>
+      </div>
+    </div>
+   
+    <div class="col-4">
+      <div class="card">
+          <img src="img/card/card2.jpg" class="card-img-top" alt="CMAT">
+          <div class="card-body">
+            <h5 class="card-title">Board/University:</h5>
+            <p class="card-text">TU (Tribhuvan University)</p>
+            <a href="login.php" class="btn btn-primary">Login</a>
+          </div>
+      </div>
+    </div>
+
+    <div class="col-4">
+      <div class="card">
+          <img src="img/card/card2.jpg" class="card-img-top" alt="CMAT">
+          <div class="card-body">
+            <h5 class="card-title">Board/University:</h5>
+            <p class="card-text">TU (Tribhuvan University)</p>
+            <a href="login.php" class="btn btn-primary">Login</a>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
